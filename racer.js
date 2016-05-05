dccee8c4d4af8b4d4f38310a0d6bef4c510cf1b0
MichaelDong32 <mjdong32@gmail.com>
2016-05-05 22:06:10 +1200

    debugging Javascript

--- racer.js
+++ racer.js
@@ -1,8 +1,8 @@
-//document.addEventListener('DOMContentLoaded',function() {
+document.addEventListener('DOMContentLoaded',function() {
 var player1 = document.getElementById('player1-strip');
 var player2 = document.getElementById('player2.strip');
-  console.log(player1);
-  console.log (player2);
+  //console.log(player1);
+  //console.log (player2);
 
 var player1_items = player1.querySelectorAll('td');
 var player2_items = player2.querySelectorAll('td');
@@ -51,13 +51,9 @@ function updatePlayerPosition (player) {
 document.onKeyUp = function whichKey(event){
   if (event.which === 81) {
     updatePlayerPosition(player1_items)
-  }else if (event.which === 80)
+  } else if (event.which === 80) {
     updatePlayerPosition(player2_items)
-  }else{
-    alert ('Press Q to move player 1 and P to move player 2')
+  }else {
+    alert('press Q or P');
   };
 };
-  
-  //});
-
-
