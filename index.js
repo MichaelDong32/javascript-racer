3df45ff8bf814d68ea1467c0c8e131928f00be39
MichaelDong32 <mjdong32@gmail.com>
2016-05-07 07:17:49 +1200

    working racer example

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLaoded', function () {
+document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('keyup', handleKeyPress)
 })
 
 function handleKeyPress (e) {
-  if (e.which === 81) {
+  if (e.which == 81) {
     movePlayer(1)
   }
 
-  if (e.which === 80) {
+  if (e.which == 80) {
     movePlayer(2)
   }
 }
@@ -17,6 +17,15 @@ function movePlayer (playerInt) {
   var cell = document.getElementsByClassName('active' + playerInt)
   var nextCell = row.cells[cell[0].cellIndex + 1]
 
+  checkForVictory(nextCell, playerInt)
+
   cell[0].className = ''
   nextCell.className = 'active' + playerInt
+}
+
+function checkForVictory (nextCell, playerInt) {
+  if (nextCell === undefined) {
+    alert('Player ' + playerInt + ' wins!')
+    window.location.reload()
+  }
 }
